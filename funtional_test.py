740cde6dc833c23313c7650d48b6ba25bebeba30
bahaha56 <clementcheng56@gmail.com>
2016-12-04 00:11:36 +0800

    Front page HTML now generated from a template

--- funtional_test.py
+++ funtional_test.py
@@ -7,7 +7,7 @@ class NewVisitorTest(unittest.TestCase):
 
 	def setUp(self):
 		self.browser = webdriver.Chrome()
-		self.browser.implicitly_wait(3)
+		#self.browser.implicitly_wait(3)
 
 	def tearDown(self):
 		self.browser.quit()
@@ -17,7 +17,7 @@ class NewVisitorTest(unittest.TestCase):
 
 		self.assertIn('To-Do', self.browser.title)
 
-		header_text = self.browser.find_element_by_tag_name('h1').header_text
+		header_text = self.browser.find_element_by_tag_name('h1').text
 		self.assertIn('To-Do', header_text)
 
 		inputbox = self.browser.find_element_by_id('id_new_item')
@@ -30,7 +30,7 @@ class NewVisitorTest(unittest.TestCase):
 		inputbox.send_keys(Keys.ENTER)
 
 		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_element_by_tag_name('tr')
+		rows = table.find_elements_by_tag_name('tr')
 		self.assertTrue(
 			any(row.text == '1: Buy peacock feathers' for row in rows)
 		)
