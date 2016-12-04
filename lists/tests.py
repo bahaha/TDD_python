4fcd0737bf8b98664ec0da49fde437fa4818df55
bahaha56 <clementcheng56@gmail.com>
2016-12-04 12:37:22 +0800

    App can save POST items now, and displays items

--- lists/tests.py
+++ lists/tests.py
@@ -6,6 +6,8 @@ from django.template.loader import render_to_string
 
 from django.http import HttpRequest
 
+from lists.models import Item
+
 # Create your tests here.
 class HomepageTest(TestCase):
 
@@ -26,7 +28,49 @@ class HomepageTest(TestCase):
 		request.POST['item_text'] = 'A new list item'
 
 		response = homepage(request)
-		self.assertIn('A new list item', response.content.decode())
 
-		expected_html = render_to_string('index.html', {'new_item_text': 'A new list item'})
-		self.assertEqual(response.content.decode(), expected_html)
+		self.assertEqual(Item.objects.count(), 1)
+		new_item = Item.objects.first()
+		self.assertEqual(new_item.text, 'A new list item')
+
+		#self.assertIn('A new list item', response.content.decode())
+
+		#expected_html = render_to_string('index.html', {'new_item_text': 'A new list item'})
+		#self.assertEqual(response.content.decode(), expected_html)
+	def test_homepage_redirects_after_POST(self):
+		request = HttpRequest()
+		request.method = 'POST'
+		request.POST['item_text'] = 'A new list item'
+		response = homepage(request)
+
+		self.assertEqual(response.status_code, 302)
+		self.assertEqual(response['location'], '/')
+
+	def test_homepage_displays_all_list_items(self):
+		Item.objects.create(text='itemey 1')
+		Item.objects.create(text='itemey 2')
+
+		request = HttpRequest()
+		response = homepage(request)
+
+		self.assertIn('itemey 1', response.content.decode())
+		self.assertIn('itemey 2', response.content.decode())
+
+class ItemModelTest(TestCase):
+
+	def test_saving_and_retrieving_items(self):
+		first_item = Item()
+		first_item.text = 'The first (ever) list item'
+		first_item.save()
+
+		second_item = Item()
+		second_item.text = 'The second list item'
+		second_item.save()
+
+		saved_items = Item.objects.all()
+		self.assertEqual(saved_items.count(), 2)
+
+		first_saved_item = saved_items[0]
+		second_saved_item = saved_items[1]
+		self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+		self.assertEqual(second_saved_item.text, 'The second list item')
--- lists/views.py
+++ lists/views.py
@@ -1,9 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from django.http import HttpResponse
+
+from lists.models import Item
 
 # Create your views here.
 def homepage(request):
 	if request.method == 'POST':
-		return render(request, 'index.html', {'new_item_text': request.POST.get('item_text', '')})
-	return render(request, 'index.html')
+		item = Item()
+		item.text = request.POST.get('item_text', '')
+		item.save()
+		return redirect('/')
+	return render(request, 'index.html', {
+		'items': Item.objects.all()
+	})
